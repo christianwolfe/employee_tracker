ebaf364461c8aa8521a76cdb7726c48bc9b9e31a
Christian Wolfe <cwolfe.k@gmail.com>
2020-07-16 17:27:03 -0400

    built out readme.md a bit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # employee_tracker
 Unit 12 MySQL Homework: Employee Tracker
+
+## Description
+The primary objective of this assignment is to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
+
+## Dependencies
+```mysql```
+```inquirer```
+```console.table```
+
+## Installation
+```npm i```
+
+## Usage
+This is a peronsal homework assignment for the UCF MERN Stack Bootcamp.
+
+## Screenshots
--- index.js
+++ index.js
@@ -20,4 +20,26 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    mainMenu();
 });
+
+function mainMenu() {
+    inquirer
+        .prompt({
+            name: "postOrBid",
+            type: "list",
+            message: "Would you like to [POST] an auction or [BID] on an auction?",
+            choices: ["POST", "BID", "EXIT"]
+        })
+        .then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.postOrBid === "POST") {
+                postAuction();
+            }
+            else if (answer.postOrBid === "BID") {
+                bidAuction();
+            } else {
+                connection.end();
+            }
+        });
+}
